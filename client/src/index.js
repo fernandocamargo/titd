a7322195bbd76461553be14ea4f0e3712ee8cbd7
Fernando Camargo Del Buono <f.camargo@expertlead.de>
2020-09-19 00:14:07 +0200

    Bring back strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
 
 import { Root } from 'components';
 
 export const initialize = () =>
-  render(<Root />, document.getElementById('root'));
+  render(
+    <StrictMode>
+      <Root />
+    </StrictMode>,
+    document.getElementById('root')
+  );
 
 export default document.fonts.ready.then(initialize);
