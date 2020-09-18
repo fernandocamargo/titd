627087c611b9f7f857684814e537621a28f5dfa1
Fernando Camargo Del Buono <f.camargo@expertlead.de>
2020-09-19 01:13:15 +0200

    Add router provider

--- client/src/components/root/render.js
+++ client/src/components/root/render.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider as Theming } from 'styled-components';
 
 import theme from 'themes';
@@ -6,9 +7,11 @@ import { App, Debugger, Style } from 'components';
 
 export default () => (
   <Debugger>
-    <Theming theme={theme}>
-      <Style />
-      <App />
-    </Theming>
+    <Router>
+      <Theming theme={theme}>
+        <Style />
+        <App />
+      </Theming>
+    </Router>
   </Debugger>
 );
