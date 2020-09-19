aceeb9ddd3f9701e2f898414f2529a3f4dd8a480
Fernando Camargo Del Buono <f.camargo@expertlead.de>
2020-09-19 13:38:36 +0200

    Expose <Routes /> component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as App } from './app';
 export { default as Debugger } from './debugger';
 export { default as Root } from './root';
+export { default as Routes } from './routes';
 export { default as Style } from './style';
