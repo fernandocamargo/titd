e549964ed003d0d7053e33ef01e41a30c544b0f1
Fernando Camargo Del Buono <f.camargo@expertlead.de>
2020-09-22 01:23:51 +0200

    Enable lazy loading on <Routes /> component

--- client/src/components/routes/render.js
+++ client/src/components/routes/render.js
@@ -1,1 +1,10 @@
-export { Switch as default } from 'react-router-dom';
+import React, { Suspense as OnDemand } from 'react';
+import { Switch } from 'react-router-dom';
+
+import { Loader } from 'components/widgets';
+
+export default ({ children }) => (
+  <OnDemand fallback={<Loader />}>
+    <Switch>{children}</Switch>
+  </OnDemand>
+);
