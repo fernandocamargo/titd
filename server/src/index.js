b8d26af18e51dd22b919b4a120990a6ad3aff322
Fernando Camargo Del Buono <f.camargo@expertlead.de>
2020-09-22 01:38:44 +0200

    Remove unnecessary methods and implement initial Github flow

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,34 @@
+import { config } from 'dotenv';
+import { stringify } from 'querystring';
+import { post } from 'axios';
 import express from 'express';
 
-export const PORT = 1337;
+export const {
+  parsed: { PORT, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET },
+} = config();
 
-export const welcome = () => console.log(`Welcome @ http://localhost:${PORT}`);
+export const ping = (_, response) =>
+  response.redirect(
+    `https://github.com/login/oauth/authorize?${stringify({
+      client_id: GITHUB_CLIENT_ID,
+      scope: ['repo'],
+    })}`
+  );
 
-export const timestamp = (_, response) =>
-  response.json({ value: new Date().getTime() });
+export const pong = ({ query: { code } }, response) =>
+  post(
+    'https://github.com/login/oauth/access_token',
+    {
+      client_id: GITHUB_CLIENT_ID,
+      client_secret: GITHUB_CLIENT_SECRET,
+      code,
+    },
+    { headers: { accept: 'application/json' } }
+  ).then(({ data }) =>
+    response.redirect(`http://localhost:3000/login?${stringify(data)}`)
+  );
 
 export default express()
-  .get('/timestamp', timestamp)
-  .listen(PORT, welcome);
+  .get('/github/ping', ping)
+  .get('/github/pong', pong)
+  .listen(PORT);
