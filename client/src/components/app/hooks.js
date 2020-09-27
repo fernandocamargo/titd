c6fa27f2ac9a10df2a38b49e9e86f4f6674bef1a
Fernando Camargo Del Buono <f.camargo@expertlead.de>
2020-09-27 12:31:07 +0200

    Move logout method into useAuthentication hook

--- client/src/components/app/hooks.js
+++ client/src/components/app/hooks.js
@@ -1,22 +1,12 @@
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 
 import { useAuthentication, useProfile } from 'hooks';
 
 export default () => {
-  const { logout: clear, logged } = useAuthentication();
+  const { logged } = useAuthentication();
   const { fetch } = useProfile();
-  const logout = useCallback(
-    event => {
-      event.preventDefault();
-
-      return clear();
-    },
-    [clear]
-  );
 
   useEffect(() => {
     logged && fetch();
   }, [logged, fetch]);
-
-  return { logout };
 };
