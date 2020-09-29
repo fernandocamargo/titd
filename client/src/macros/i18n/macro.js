0f6e1d4328f99b5edb5081c96f328929057bd4b7
Fernando Camargo Del Buono <f.camargo@expertlead.de>
2020-09-29 17:20:50 +0200

    Add macro for improving  react-intl's API

---
+++ client/src/macros/i18n/macro.js
@@ -0,0 +1,52 @@
+const { dirname, relative, resolve } = require('path');
+const { cwd } = require('process');
+const { createMacro } = require('babel-plugin-macros');
+const {
+  compilerOptions: { baseUrl },
+} = require('../../../jsconfig.json');
+
+module.exports = createMacro(
+  ({
+    babel: {
+      types: {
+        callExpression,
+        identifier,
+        importDeclaration,
+        importDefaultSpecifier,
+        objectExpression,
+        objectProperty,
+        stringLiteral,
+      },
+    },
+    state: {
+      file: {
+        path: {
+          node: { body },
+        },
+      },
+      filename,
+    },
+    references: { defineMessages },
+  }) => {
+    defineMessages.forEach(({ parentPath }) => {
+      const [{ node: messages }] = parentPath.get('arguments');
+      const namespace = relative(resolve(cwd(), baseUrl), dirname(filename));
+
+      body.unshift(
+        importDeclaration(
+          [importDefaultSpecifier(identifier('defineMessages'))],
+          stringLiteral('helpers/i18n/define-messages')
+        )
+      );
+
+      return parentPath.replaceWith(
+        callExpression(identifier('defineMessages'), [
+          objectExpression([
+            objectProperty(identifier('namespace'), stringLiteral(namespace)),
+            objectProperty(identifier('messages'), messages),
+          ]),
+        ])
+      );
+    });
+  }
+);
